e4f5f568ad8fa314c8aefefb15d204e20e87b040
Mohammad Hosseingholizadeh <m.hgzahdeh@gmail.com>
2021-09-25 12:51:29 +0200

    web crawling part 3- find the number of adv in different cities

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
 
 
 def get_page(url, start=0):
     try:
-        response = requests.get(url.format(str(start)))
+        response = requests.get(url + str(start))
     except:
         return None
-    print(response.status_code, response.url)
+    # print(response.status_code, response.url)
     return response
 
 
 def get_site_data(html_doc):
-    return BeautifulSoup(html_doc)
+    return BeautifulSoup(html_doc, "html.parser")
 
 
 def total_num_adv(link):
@@ -25,7 +26,7 @@ def find_links(soup):
     return soup.find_all('a', attrs={'class': 'hdrlnk'})
 
 
-def start_crawl(link):
+def start_crawl_city(link):
     start = 0
     crawl = True
     adv_links = list()
@@ -37,11 +38,25 @@ def start_crawl(link):
         adv_links.extend(new_links)
         start += 120
         crawl = bool(len(new_links))
-    adv_links = adv_links[:adv_num-1]
+    adv_links = adv_links[:adv_num]
     return adv_links
 
 
+def start_crawl():
+    cities = ['paris', 'berlin', 'amsterdam', 'munich']
+    link = "https://{}.craigslist.org/d/housing/search/hhh?lang=en&cc=gb&s="
+    for city in cities:
+        links = start_crawl_city(link.format(city))
+        print(f'Total adv in {city.capitalize()}:',  len(links))
+
+
+def get_pages_data():
+    print('Not Implemented Function')
+
+
 if __name__ == "__main__":
-    link = "https://paris.craigslist.org/d/housing/search/hhh?lang=en&cc=gb&s={}"
-    links = start_crawl(link)
-    print('total:',  len(links))
+    switch = sys.argv[1]
+    if switch == "find_links":
+        start_crawl()
+    elif switch == "extract_pages":
+        get_pages_data()
